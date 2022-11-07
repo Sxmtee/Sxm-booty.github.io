66d91a138d3e4dc1fa52f9a38d935ddf506eb852
Sxmtee <somtoo76@gmail.com>
2022-11-07 21:43:36 +0100

    prompt

--- Prompt Assignment/Intern.js
+++ Prompt Assignment/Intern.js
@@ -1,30 +1,5 @@
-let interndb = prompt("Enter Name of PMT Intern")
-if (interndb == "Somto" || interndb == "Obi" || interndb == "Somto Obi") {
-    alert("Somto Obi")
-}else if (interndb == "Jerry" || interndb == "Ojimadu" || interndb == "Jerry Ojimadu") {
-    alert("Jerry Ojimadu")
-}else if (interndb == "Nwefuru" || interndb == "James" || interndb == "Nwefuru James") {
-    alert("Nwefuru James")
-}else if (interndb == "Omenoke" || interndb == "Chidera" || interndb == "Omenoke Chidera") {
-    alert("Omenoke Chidera")
-}else if (interndb == "Nwadilibe" || interndb == "Chukwuemeka" || interndb == "Nwadilibe Chukwuemeka") {
-    alert("Nwadilibe Chukwuemeka")
-}else if (interndb == "Anachunam" || interndb == "Augustine" || interndb == "Anachunam Augustine") {
-    alert("Anachunam Augustine")
-}else if (interndb == "Nwamuta" || interndb == "Raphael" || interndb == "Nwamuta Raphael") {
-    alert("Nwamuta Raphael")
-}else if (interndb == "Victory" || interndb == "Amobi" || interndb == "Victory Amobi") {
-    alert("Victory Amobi")
-}else if (interndb == "Didigwu" || interndb == "Paul" || interndb == "Didigwu Paul") {
-    alert("Didigwu Paul")
-}else if (interndb == "Ugwuanyi" || interndb == "Diamond" || interndb == "Ugwuanyi Diamond") {
-    alert("Ugwuanyi Diamond")
-}else if (interndb == "Chigozie" || interndb == "Peters" || interndb == "Chigozie Peters") {
-    alert("Chigozie Peters")
-}else if (interndb == "Onyemachi" || interndb == "Solomon" || interndb == "Onyemachi Solomon") {
-    alert("Onyemachi Solomon")
-}else if (interndb == "Uban" || interndb == "Samuel" || interndb == "Uban Samuel") {
-    alert("Uban Samuel")
-}else{
-    alert("Not available, check next bootcamp edition")
+const icon = document.querySelector('.icon');
+const search = document.querySelector('.search');
+icon.onclick = function() {
+    search.classList.toggle('active')
 }
